9edc153eaa8d2d2d9dc828422ead8990b13af9f6
atlas <a.sproson@outlook.com>
2022-07-10 13:53:12 +0100

    Adds initial layout and input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react"
 
 function App() {
+
+  const [searchPokemon, setSearchPokemon] = useState()
+
+  const handleSearchPokemon = e => {
+    setSearchPokemon(e.target.value)
+  }
+
   return (
     <div className="App">
-
+      <div>
+        <h1>Pokémon Stats</h1>
+      </div>
+      <div>
+        <input type="text" placeholder="Search..." onChange={handleSearchPokemon}></input>
+      </div>
+      <button>Submit</button>
     </div>
   );
 }
